1d1108d9dc96099a8901db922c0a2e41aaec3dd9
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-05 16:26:13 +0200

    17. Understanding methods in Vue Apps

--- l014-basics/app.js
+++ l014-basics/app.js
@@ -5,6 +5,15 @@ const app = Vue.createApp({
             vueLink: "https://vuejs.org"
         };
     },
+    methods: {
+        outputGoal() {
+            const randomNo = Math.random();
+            if (randomNo < 0.5)
+                return "Learn Vue!";
+            else
+                return "Master Vue!";
+        },
+    },
 });
 
 app.mount("#user-goal");
