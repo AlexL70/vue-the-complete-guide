7d3fb8b2104c68b7a7c09ab6ef19f55b18a761d2
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-07 11:48:36 +0200

    41. Rendering Content Conditionally

--- l040-rendering/app.js
+++ l040-rendering/app.js
@@ -1,7 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: [],
+      currentGoalValue: "",
+    };
   },
+  methods: {
+    addGoal() {
+      if (this.currentGoalValue.length > 0) {
+        this.goals.push(this.currentGoalValue);
+        this.currentGoalValue = "";
+      }
+    }
+  }
 });
 
 app.mount('#user-goals');
