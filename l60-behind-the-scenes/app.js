ac543109ed65037ec7f5a8e230d8785b40e13b18
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-08 10:45:00 +0200

    64. Working with Refs

--- l60-behind-the-scenes/app.js
+++ l60-behind-the-scenes/app.js
@@ -1,16 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
+      //currentUserInput: '',
       message: 'Vue is great!',
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
+    //saveInput(event) {
+    //  this.currentUserInput = event.target.value;
+    //},
     setText() {
-      this.message = this.currentUserInput;
+      this.message = this.$refs.userInput.value;
+      //this.message = this.currentUserInput;
     },
   },
 });
