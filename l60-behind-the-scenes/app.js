1458b00405551b1583be30fe8d6c079a9fbb31fc
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-08 10:00:47 +0200

    61. Vue Reactivity: A Deep Dive

--- l60-behind-the-scenes/app.js
+++ l60-behind-the-scenes/app.js
@@ -16,3 +16,29 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const data = {
+  message: "Hello!",
+  longMessage: "Hello! World!",
+};
+
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+    if (key == "message")
+      target.longMessage = `${value} World!`
+  }
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = "Hello!!!";
+console.log(proxy.longMessage);
+
+//let message = "Hello!";
+//let longMessage = message + "World!";
+//console.log(longMessage);
+//message = "Hello!!!!!! "
+//console.log(longMessage);
