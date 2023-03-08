931bcc5ea71d142b78be094e4a2fca01311135ac
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-08 18:39:33 +0200

    85. Creating a Basic Vue App

---
+++ l079-working-with-cli/vue-first-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li>Manuel Lorenz</li>
+        </ul>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    data() {
+        return {
+            friends: [
+                {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "1234 666 777",
+                    email: "manuel@localhost.com",
+                },
+                {
+                    id: "julie",
+                    name: "Julie Jones",
+                    phone: "9999 333 555",
+                    email: "julie@localhost.com",
+                }
+            ],
+        };
+    }
+});
+</script>
