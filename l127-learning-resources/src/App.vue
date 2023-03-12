b8286b69c91ac90abda73522d69e23a37966c3f4
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-12 18:50:26 +0200

    127. Setup & First Steps

---
+++ l127-learning-resources/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+        <li v-for="r in stroredResources" :key="r.id">
+            <a :href="r.link">{{ r.title }}</a>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { Resource } from "./types/DtoInterfaces"
+
+export default defineComponent({
+    name: "App",
+    data() {
+        return {
+            stroredResources: [
+                {
+                    id: "official-guide",
+                    title: "Official guide",
+                    description: "The official Vue.js documentation",
+                    link: "https://vuejs.org",
+                },
+                {
+                    id: "google",
+                    title: "Google",
+                    description: "Learn to google...",
+                    link: "https://google.com",
+                }
+            ] as Array<Resource>,
+        };
+    },
+});
+</script>
