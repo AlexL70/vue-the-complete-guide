d197077048ffa93420f9d62b53f4352fa1aa14d5
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-12 15:39:09 +0200

    116. Scoped Slots

--- l109-diving-deeper-into-components/src/App.vue
+++ l109-diving-deeper-into-components/src/App.vue
@@ -4,6 +4,14 @@
     <!--the-header></!--the-header-->
     <badge-list></badge-list>
     <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <course-goals>
+      <!-- for default slot, template here is not necessary; #default attribute
+      could have been just used on parent (<course-goal>) element -->
+      <template #default="slotProps">
+        <h3>{{ slotProps.item }}</h3>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -11,6 +19,7 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from "./components/CourseGoals.vue";
 export default {
   data() {
     return {
@@ -22,7 +31,7 @@ export default {
     };
   },
   components: {
-    TheHeader, UserInfo, BadgeList
+    TheHeader, UserInfo, BadgeList, CourseGoals,
   },
 };
 </script>
