8577bdfc4407b99e30b4eb2ab277c8bacedea14c
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-12 17:40:11 +0200

    121. Working with Fragments

--- l109-diving-deeper-into-components/src/components/ManageGoals.vue
+++ l109-diving-deeper-into-components/src/components/ManageGoals.vue
@@ -1,16 +1,14 @@
 <template>
-    <div>
-        <h2>Manage Goals</h2>
-        <input type="text" ref="goal" />
-        <button @click="setGoal">Set Goal</button>
-        <teleport to="body">
-            <error-alert v-if="inputIsInvalid">
-                <h2>Input is invalid.</h2>
-                <p>Manage goal can never be empty! Please add some value!</p>
-                <button @click="confirmError">OK</button>
-            </error-alert>
-        </teleport>
-    </div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+        <error-alert v-if="inputIsInvalid">
+            <h2>Input is invalid.</h2>
+            <p>Manage goal can never be empty! Please add some value!</p>
+            <button @click="confirmError">OK</button>
+        </error-alert>
+    </teleport>
 </template>
 
 <script>
