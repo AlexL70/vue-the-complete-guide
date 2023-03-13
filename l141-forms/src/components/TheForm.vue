e664ce2fd84ec5730cfa776d571b9f5cae415960
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-13 20:46:40 +0200

    144. Using v-model with Checkboxes & Radiobuttons

--- l141-forms/src/components/TheForm.vue
+++ l141-forms/src/components/TheForm.vue
@@ -19,32 +19,36 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest" />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest" />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest" />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video-courses" v-model="how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="others" v-model="how" />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
+      <label for="confirm-terms">Agree to terms of use?</label>
     </div>
     <div>
       <button>Save Data</button>
@@ -59,6 +63,9 @@ export default {
       userName: "",
       userAge: null,
       referrer: "google",
+      interest: [],
+      how: null,
+      confirm: false,
     };
   },
   methods: {
@@ -71,6 +78,12 @@ export default {
       this.userAge = null;
       console.log(`Referrer: ${this.referrer}`);
       this.referrer = "google";
+      console.log(`Interest: ${this.interest}`);
+      this.interest = [];
+      console.log(`How: ${this.how}`);
+      this.how = null;
+      console.log(`Confirm: ${this.confirm}`);
+      this.confirm = false;
     }
   },
 }
