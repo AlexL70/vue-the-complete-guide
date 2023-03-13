aa2875d660bcbbf448cfe64f4cb0b2687d5314e3
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-13 12:49:36 +0200

    131. Adding a Base Button

---
+++ l127-learning-resources/src/components/UI/BaseButton.vue
@@ -0,0 +1,52 @@
+<template>
+    <button :type="type" :class="mode">
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+export default defineComponent({
+    props: {
+        type: {
+            type: Object as PropType<"submit" | "reset" | "button">,
+            required: false,
+            default: "button",
+        },
+        mode: {
+            type: String,
+            required: false,
+            default: "",
+        }
+    },
+});
+</script>
+
+<style scoped>
+button {
+    padding: 0.75rem 1.5rem;
+    font-family: inherit;
+    background-color: #3a0061;
+    border: 1px solid #3a0061;
+    color: white;
+    cursor: pointer;
+}
+
+button:hover,
+button:active {
+    background-color: #270041;
+    border-color: #270041;
+}
+
+.flat {
+    background-color: transparent;
+    color: #3a0061;
+    border: none;
+}
+
+.flat:hover,
+.flat:active {
+    background-color: #edd2ff;
+}
+</style>
