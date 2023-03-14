6217c2ab052e03bd5294bcda05fbb2a58b093d10
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-14 15:55:27 +0200

    161. Handling Technical / Browser-side Errors

--- l151-http-requests/src/components/survey/UserExperiences.vue
+++ l151-http-requests/src/components/survey/UserExperiences.vue
@@ -6,6 +6,7 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <h2 v-if="isLoading">Loading data...</h2>
+      <h5 v-else-if="errorMessage">{{ errorMessage }}</h5>
       <ul v-else-if="results && results.length !== 0">
         <survey-result v-for="result in results" :key="result.id" :name="result.name"
           :rating="result.rating"></survey-result>
@@ -26,11 +27,13 @@ export default {
     return {
       results: [],
       isLoading: false,
+      errorMessage: null,
     };
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.errorMessage = null;
       fetch("https://vue-http-demo-d9ba8-default-rtdb.europe-west1.firebasedatabase.app/surveys.json")
         .then(response => {
           console.log(response);
@@ -44,6 +47,10 @@ export default {
             results.push({ id: id, name: data[id].name, rating: data[id].rating });
           }
           this.results = results;
+          this.isLoading = false;
+        }).catch(err => {
+          console.log(`Error occurred while fetching data: ${err}`);
+          this.errorMessage = err;
           this.isLoading = false;
         });
     },
