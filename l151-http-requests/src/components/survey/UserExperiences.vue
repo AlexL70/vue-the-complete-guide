f4002beaf6eec325704e6bff3066d97b8737a405
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-14 15:21:59 +0200

    157. Getting Data (GET Request) & Transforming Response Data

--- l151-http-requests/src/components/survey/UserExperiences.vue
+++ l151-http-requests/src/components/survey/UserExperiences.vue
@@ -3,15 +3,11 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name"
+          :rating="result.rating"></survey-result>
       </ul>
     </base-card>
   </section>
@@ -21,9 +17,31 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch("https://vue-http-demo-d9ba8-default-rtdb.europe-west1.firebasedatabase.app/surveys.json")
+        .then(response => {
+          console.log(response);
+          if (response.ok) {
+            return response.json();
+          }
+        }).then(data => {
+          console.log(data);
+          const results = [];
+          for (const id in data) {
+            results.push({ id: id, name: data[id].name, rating: data[id].rating });
+          }
+          this.results = results;
+        });
+    },
   },
 };
 </script>
