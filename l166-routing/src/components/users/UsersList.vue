3e998c833af9f6a14479204818d2e0bd28b36f6d
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-15 15:50:14 +0200

    186. Beyond Entering: Route Leave Guards

--- l166-routing/src/components/users/UsersList.vue
+++ l166-routing/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="emulateSavingChanges">Save changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,17 +14,38 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
   methods: {
     confirmInput() {
       // do something important
       // need to navigate to teams
       this.$router.push({ name: "teams", query: { sort: "asc" } });
     },
+    emulateSavingChanges() {
+      this.changesSaved = true;
+    },
   },
   beforeRouteEnter(to, from, next) {
     console.log("Before entering UserList component.", to, from);
     next();
   },
+  beforeRouteLeave(to, from, next) {
+    console.log("UsersList component before route leave.");
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      let wantsToLeave = confirm("Are you sure you want to leave the page? All unsaved data will be lost!");
+      next(wantsToLeave);
+    }
+  },
+  unmounted() {
+    console.log("Unmounted UserList component");
+  }
 };
 </script>
 
