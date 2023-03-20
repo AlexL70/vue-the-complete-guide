f445311769b35fa5cc784645f950c8472f5729e9
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-20 22:16:26 +0200

    244. Registering as a Coach: The Form

--- l235-find-coach/src/pages/coaches/CoachRegistration.vue
+++ l235-find-coach/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,18 @@
 <template>
-    <h2>Register New Coach</h2>
+    <section>
+        <base-card>
+            <h2>Register as a coach now!</h2>
+            <coach-form></coach-form>
+        </base-card>
+    </section>
 </template>
+
+<script lang="ts">
+import CoachForm from "@/components/coaches/CoachForm.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+    components: {
+        CoachForm,
+    }
+});
+</script>
