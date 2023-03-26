d1e4d0c5b3e9610587be78e983965d57ca2b3b91
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-26 18:16:47 +0300

    266. Better UX: Loading Spinner & Error Handling

--- l235-find-coach/src/pages/auth/UserAuth.vue
+++ l235-find-coach/src/pages/auth/UserAuth.vue
@@ -1,20 +1,31 @@
 <template>
-    <base-card>
-        <form @submit.prevent="submitForm">
-            <div class="form-control">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" v-model.trim="loginData.email">
-            </div>
-            <div class="form-control">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" v-model.trim="loginData.password">
-            </div>
-            <p v-if="!formIsValid" class="error">Invalid data. Email must be a valid email and password must be at least 6
-                characters long. Please fix it and try again.</p>
-            <base-button>{{ submitButtonCaption }}</base-button>
-            <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
-        </form>
-    </base-card>
+    <div>
+        <base-dialog :show="isError" title="An error occured" @close="handleError">
+            <p>{{ error }}</p>
+        </base-dialog>
+        <base-dialog :show="isLoading" title="Please, wait." fixed>
+            <p v-if="mode === 'login'">Authenticating...</p>
+            <p v-else>Signing up new user...</p>
+            <base-spinner></base-spinner>
+        </base-dialog>
+        <base-card>
+            <form @submit.prevent="submitForm">
+                <div class="form-control">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" v-model.trim="loginData.email">
+                </div>
+                <div class="form-control">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" v-model.trim="loginData.password">
+                </div>
+                <p v-if="!formIsValid" class="error">Invalid data. Email must be a valid email and password must be at least
+                    6
+                    characters long. Please fix it and try again.</p>
+                <base-button>{{ submitButtonCaption }}</base-button>
+                <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
+            </form>
+        </base-card>
+    </div>
 </template>
 
 <script lang="ts">
@@ -31,6 +42,8 @@ export default defineComponent({
             } as LoginData,
             formIsValid: true,
             mode: "login" as ("login" | "signup"),
+            isLoading: false as boolean,
+            error: null as (string | null),
         };
     },
     computed: {
@@ -51,23 +64,34 @@ export default defineComponent({
                     return "Login intead";
             }
         },
+        isError() {
+            return this.error != null && this.error.length > 0;
+        }
     },
     methods: {
-        submitForm() {
+        async submitForm() {
             if (this.loginData.email === "" || !this.loginData.email.includes("@") || this.loginData.password.length < 6) {
                 this.formIsValid = false;
                 return;
             }
-            switch (this.mode) {
-                case "login":
-                    //  TODO – add loging code
-                    break;
+            this.isLoading = true;
+            let errMsg = "";
+            try {
+                switch (this.mode) {
+                    case "login":
+                        errMsg = "Failed to authenticate user.";
+                        //  TODO – add login code
+                        break;
 
-                case "signup":
-                    this.userStore.signup(this.loginData);
-                    break;
+                    case "signup":
+                        errMsg = "Failed to sign up user.";
+                        await this.userStore.signup(this.loginData);
+                        break;
+                }
+            } catch (err: any) {
+                this.error = err.message ?? errMsg;
             }
-            //  send http request for login/signup
+            this.isLoading = false;
         },
         switchAuthMode() {
             if (this.mode === "login")
@@ -75,6 +99,9 @@ export default defineComponent({
             else
                 this.mode = "login";
         },
+        handleError() {
+            this.error = null;
+        }
     }
 });
 </script>
