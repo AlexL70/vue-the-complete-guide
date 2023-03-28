9806b8f1948f23b3038170dabdad57c5425b1350
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-28 17:13:34 +0300

    278. Optimization: Using Asynchronous Components

--- l235-find-coach/src/router/index.ts
+++ l235-find-coach/src/router/index.ts
@@ -1,12 +1,24 @@
+import { defineAsyncComponent } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import userStore from "@/store/user";
-import CoachDetail from "@/pages/coaches/CoachDetail.vue";
 import CoachesList from "@/pages/coaches/CoachesList.vue";
-import CoachRegistration from "../pages/coaches/CoachRegistration.vue";
-import ContactCoach from "@/pages/requests/ContactCoach.vue";
-import RequestsReceived from "@/pages/requests/RequestsReceived.vue";
-import UserAuth from "@/pages/auth/UserAuth.vue";
 import NotFound from "@/pages/NotFound.vue";
+
+const CoachDetail = defineAsyncComponent(
+  () => import("@/pages/coaches/CoachDetail.vue")
+);
+const CoachRegistration = defineAsyncComponent(
+  () => import("@/pages/coaches/CoachRegistration.vue")
+);
+const ContactCoach = defineAsyncComponent(
+  () => import("@/pages/requests/ContactCoach.vue")
+);
+const RequestsReceived = defineAsyncComponent(
+  () => import("@/pages/requests/RequestsReceived.vue")
+);
+const UserAuth = defineAsyncComponent(
+  () => import("@/pages/auth/UserAuth.vue")
+);
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
