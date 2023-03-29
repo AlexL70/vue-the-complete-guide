caea760fcb5261738c316d28a3e049807906ca25
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-29 16:49:42 +0300

    296. Emitting Custom Events

--- l285-composition-api/src/components/UserData.vue
+++ l285-composition-api/src/components/UserData.vue
@@ -1,16 +1,21 @@
 <template>
-    <h2>{{ fullName }}</h2>
-    <h3>{{ user.age }}</h3>
+    <div>
+        <h2>{{ fullName }}</h2>
+        <h3>{{ user.age }}</h3>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, type PropType } from "vue";
 import type { User } from "@/types";
 export default defineComponent({
-    setup(props) {
+    setup(props, context) {
         const fullName = computed(() => {
             return `${props.user.firstName}${props.user.firstName.length > 0 && props.user.lastName.length > 0 ? " " : ""}${props.user.lastName}`;
         });
+        console.log(context);
+        // emitting custom events from Options API
+        //context.emit("my-event", "some-argument"); // the same as this.$emit("my-event", "some-argument");
         return { fullName };
     },
     props: {
