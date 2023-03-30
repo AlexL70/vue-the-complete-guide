6df48cd5b97d26ddd0198f31b0dd72430d97a97a
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-03-30 13:57:46 +0300

    302. Migrating the Remaining Components

--- l299-migrate-option-to-composition/src/components/UI/BaseSearch.vue
+++ l299-migrate-option-to-composition/src/components/UI/BaseSearch.vue
@@ -8,10 +8,12 @@
 export default {
   props: ['searchTerm'],
   emits: ['search'],
-  methods: {
-    search(event) {
-      this.$emit('search', event.target.value);
-    },
+  setup(_, context) {
+    function search(event) {
+      context.emit('search', event.target.value);
+    }
+
+    return { search };
   },
 };
 </script>
--- l299-migrate-option-to-composition/src/components/projects/ProjectsList.vue
+++ l299-migrate-option-to-composition/src/components/projects/ProjectsList.vue
@@ -14,47 +14,51 @@
 
 <script>
 import ProjectItem from './ProjectItem.vue';
-
+import { ref, computed, watch, toRefs } from 'vue';
 export default {
   components: {
     ProjectItem,
   },
   props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
-        );
-      }
-      return this.user.projects;
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+
+    watch(enteredSearchTerm, (newValue) => {
       setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+        if (newValue === enteredSearchTerm.value) {
+          activeSearchTerm.value = newValue;
         }
       }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
+    });
+
+    // All props could be watched here because there is only one prop
+    // in this particular component. But if there were more, and you
+    // do not want to watch them all, this is the way to do it.
+    // Transform one particular property to the Ref and then watch it.
+    const { user } = toRefs(props);
+    watch(user, () => {
+      enteredSearchTerm.value = '';
+    });
+
+    const hasProjects = computed(() => {
+      return props.user.projects && availableProjects.value.length > 0;
+    });
+
+    const availableProjects = computed(() => {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter((prj) =>
+          prj.title.includes(activeSearchTerm.value)
+        );
+      }
+      return props.user.projects;
+    });
+
+    return { enteredSearchTerm, updateSearch, hasProjects, availableProjects };
   },
 };
 </script>
