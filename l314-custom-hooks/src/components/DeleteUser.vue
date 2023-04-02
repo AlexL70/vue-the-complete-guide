9f0de97ef615a03d5113602adf867d1c69f78986
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-04-02 15:09:58 +0300

    315. More Custom Composition Functions

--- l314-custom-hooks/src/components/DeleteUser.vue
+++ l314-custom-hooks/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -11,13 +11,17 @@
 <script>
 import useAlert from "@/hooks/alert.js";
 import UserAlert from './UserAlert.vue';
+import { ref } from "vue";
 
 export default {
   components: {
     UserAlert,
   },
   setup() {
-    return useAlert();
+    const alertTitle = ref("Delete User?")
+    const { alertIsVisible, showAlert, hideAlert } = useAlert(true);
+    return { alertTitle, alertIsVisible, showAlert, hideAlert };
+
   },
 };
 </script>
